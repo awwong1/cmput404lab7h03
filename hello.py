3d0a76e0aae753c84b42c84972d80f947ebb2800
Alexander Wong <awwong1@ualberta.ca>
2019-02-26 08:32:08 -0700

    basic hello world api

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route("/")
-def hello():
-    return "Hello, world!"
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return { "Hello": "world" }
+
+api.add_resource(HelloWorld, "/")
 
 if __name__ == "__main__":
     app.run(debug=True)
